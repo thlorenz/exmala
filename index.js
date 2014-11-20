1d6a1dba2266f24e503a12a9baf40e72a1f06374
Thorsten Lorenz <thlorenz@gmx.de>
2014-11-19 20:21:10 -0800

    first benchmarker

---
+++ index.js
@@ -0,0 +1,119 @@
+'use strict';
+var assert      = require('assert-cb')
+  , queue       = require('minimal-queue')
+  , fs          = require('fs')
+  , path        = require('path')
+  , ProgressBar = require('progress')
+
+function aggregate(result) {
+  var sum = 0;
+  var diffs = result.times.map(function diff_(ts) {
+    var nanoSecs1 = ts.start[0] * 1E9 + ts.start[1]
+      , nanoSecs2 = ts.end[0] * 1E9 + ts.end[1]
+
+    var diff = (nanoSecs2 - nanoSecs1) / 1E6;
+    sum += diff;
+    return diff + ' ms';
+  })
+
+  var rsss       = result.memories.map(function getRss(x) { return x.mem.rss })
+    , heapTotals = result.memories.map(function getHeapTotal(x) { return x.mem.heapTotal })
+    , heapUseds  = result.memories.map(function getHeapUsed(x) { return x.mem.heapUsed })
+
+  return {
+      averageProcessing : (sum / diffs.length) + ' ms'
+    , elapseds          : diffs
+    , heapUseds         : heapUseds
+    , heapTotals        : heapTotals
+    , rsss              : rsss
+    , memories          : result.memories
+  }
+}
+
+var go = module.exports = function benchmark(opts, cb) {
+  opts = opts || {};
+
+  var parser      = opts.parser
+    , concurrency = opts.concurrency
+    , n           = opts.number
+    , file        = opts.file
+    , interval    = opts.interval || 1000
+    , resultsFile = opts.resultsFile
+
+  if (!assert(parser, 'need parser', cb)) return;
+  if (!assert(concurrency, 'need concurrency', cb)) return;
+  if (!assert(n, 'need number', cb)) return;
+  if (!assert(file, 'need file', cb)) return;
+  if (!assert(resultsFile, 'need resultsFile', cb)) return;
+  file = path.join(__dirname, 'data', file);
+
+  var times = []
+    , memories = []
+    , bar = new ProgressBar('  processing [:bar] :percent :etas', { width: 20, total: n });
+
+  var parse = opts.streaming
+    ? require('./streaming/parse-stream')[parser]
+    : require('./non-streaming/parse-stream')[parser]
+
+  function trackMemoryUsage() {
+    if (typeof gc === 'function' && !opts.nogc) {
+      /*globals gc*/
+      gc(); gc();
+    }
+    memories.push({ time: process.hrtime(), mem: process.memoryUsage() });
+  }
+
+  function processFile() {
+    /* jshint validthis: true */
+    var self = this;
+    var stream = fs.createReadStream(file);
+    var time = { start: process.hrtime(), end: null }
+
+    parse(stream, function process_(err, res) {
+      if (err) return cb(err);
+      time.end = process.hrtime();
+      times.push(time);
+      self.done();
+      bar.tick();
+    })
+  }
+
+  function ondone() {
+    clearInterval(tok)
+
+    var result = {
+        options  : opts
+      , times    : times
+      , memories : memories
+    }
+    fs.writeFile(opts.resultsFile, JSON.stringify(aggregate(result), null, 2), 'utf8', cb)
+  }
+
+  var tok = setInterval(trackMemoryUsage, opts.interval);
+
+  var q = queue.up(processFile);
+  q.concurrency = concurrency;
+  q.allDone = ondone;
+
+  for (var i = 0; i < n; i++) {
+    q.enqueue(processFile)  
+  }
+}
+
+var opts = {
+    parser: 'xml2js'
+  , streaming: false
+  , concurrency: 200
+  , number: 400
+  , file: 'ibm-request.soap.xml'
+}
+
+opts.resultsFile = path.join(__dirname, 'results',  
+  (opts.streaming ? 'streaming' : 'non_streaming')
+  + '-' + opts.parser + '-c-'  + opts.concurrency + '-n-' + opts.number
+  + '-' + opts.file + '.json')
+
+go(opts, function (err) {
+  if (err) return console.error(err);
+  console.log('\nResults stored at', opts.resultsFile);  
+})
